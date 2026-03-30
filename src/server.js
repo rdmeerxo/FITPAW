71b0de614f67dbec53e56ec43a6df5c4cfcf2f68
rdmeerxo <erdemtugs323@gmail.com>
2026-03-30 17:14:40 +0800

    removing debug line from server.js pls work im crying

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 
 import app from './app.js';
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'SET' : 'NOT SET');
-
-
 const PORT = process.env.PORT || 3001;
 
 //railway is gonna handle HTTPS, so using plain HTTP
